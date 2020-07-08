b53b5a6e71480c1b9e56d1dafccdce7113b7bc14
Rhaegar <ashokjune17@gmail.com>
2020-07-08 16:52:17 +0530

    Add files via upload

---
+++ auth_ahea.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This plugin is to be used for AHEA User authentication.
+ *
+ * @package    auth
+ * @subpackage ahea
+ * @author     ashokjune17@gmail.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'AHEA SSO';
+$string['companyname'] = 'AHEA University';
